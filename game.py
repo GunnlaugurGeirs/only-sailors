9a27eb23e8671fa4c6810f41c7f0c5cacca31fad
Nikola Katardjiev <nikola@katardjiev.net>
2025-03-19 21:01:48 +0100

    removed unneeded CLI tools, added hook for ollama

--- game.py
+++ game.py
@@ -1,32 +1,21 @@
 import threading
-import time
 from config import read_config
-from memory_utils import get_pokemon_name, get_first_pokemon_info
 from queue import Queue
 from pyboy import PyBoy
-
-key_map = {
-    "UP": "up",
-    "DOWN": "down",
-    "LEFT": "left",
-    "RIGHT": "right",
-    "A": "a",
-    "B": "b",
-    "START": "start",
-    "SELECT": "select",
-}
+from constants import key_map
+from game_service import GameService
 
 class GameInstance:
     def __init__(self, rom_path):
-        self.pyboy = PyBoy(gamerom=rom_path, sound_volume=0)
+        self.pyboy = PyBoy(gamerom=rom_path, sound_emulated=False)
         self.command_queue = Queue()
+        self.output_queue = Queue(maxsize=1)
         self.input_thread = None
         self.output_thread = None
         self.image = None
         self.image_lock = threading.Lock()
 
     def run(self):
-        self.start_input_thread()
         self.start_output_thread()
         while self.pyboy.tick():
             if not self.command_queue.empty():
@@ -37,18 +26,6 @@ class GameInstance:
 
         self.pyboy.stop()
 
-    # TODO: remove CLI option when Agent is implemented
-    def get_inputs(self):
-        while True:
-            self.get_input()
-
-    def get_input(self):
-        command = input(
-            "Enter command (UP, DOWN, LEFT, RIGHT, A, B, START, SELECT) or 'exit' to quit: "
-        ).upper()
-
-        self.command(command)
-
     def command(self, command):
         if command == "EXIT":
             self.command_queue.put("EXIT")
@@ -57,18 +34,6 @@ class GameInstance:
         if command in key_map:
             button = key_map[command]
             self.command_queue.put(button)
-
-    def start_input_thread(self):
-        cli = read_config("Settings", "cli", default=False, value_type=bool)
-        if cli:
-            self.input_thread = threading.Thread(
-                target=self.get_inputs, args=(), daemon=True
-            )
-        else:
-            self.input_thread = threading.Thread(
-                target=self.get_input, args=(), daemon=True
-            )
-        self.input_thread.start()
     
     def start_output_thread(self):
         self.output_thread = threading.Thread(target=self.capture_image, daemon=True)
@@ -76,11 +41,11 @@ class GameInstance:
 
     def capture_image(self):
         while True:
-            with self.image_lock: 
-                self.image = self.pyboy.screen.image.copy()
-                print(get_pokemon_name(self.pyboy))
-                print(get_first_pokemon_info(self.pyboy))
-            time.sleep(read_config("Settings", "capture_time", default=5, value_type=int))
+            if self.output_queue.empty():
+                with self.image_lock: 
+                    self.image = self.pyboy.screen.image.copy()
+                self.output_queue.put((self.image, self.pyboy.game_wrapper.game_area_collision()))    
+            
 
     def get_output(self):
         return self.image, self.pyboy.game_wrapper.game_area_collision()
@@ -88,4 +53,6 @@ class GameInstance:
 if __name__ == "__main__":
     gamefile = read_config("Settings", "gamefile", default="emulation/game.gb")
     game = GameInstance(gamefile)
+    game_service = GameService(game.command_queue, game.output_queue)
+    threading.Thread(target=game_service.start_game, daemon=True).start()
     game.run()
